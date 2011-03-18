6885138d4522f0f8c1b3910ba2b430765a8086ab
Eric Wendelin <emwendelin@gmail.com>
2011-03-18 08:00:02 -0600

    Changing content type

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
   
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n");
+  res.writeHead(200, { "Content-Type": "text/json" })
+  res.end("{'options':[1,2,3,5,8,13,21,34,55,100], 'names': ['Eric Wendelin']}");
 });
   
 server.listen(process.env.PORT || 8001);
