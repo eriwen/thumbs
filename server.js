db797b802586f35af20a11bd8108a47e96ef6531
Eric Wendelin <emwendelin@gmail.com>
2011-03-19 10:20:18 -0600

    Hello express

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-var http = require('http');
-  
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/json" })
-  res.end("{'options':[1,2,3,5,8,13,21,34,55,100], 'names': ['Eric Wendelin']}");
+var app = require('express').createServer();
+
+app.get('/', function(req, res){
+  res.send('hello world');
 });
-  
-server.listen(process.env.PORT || 8001);
+
+app.listen(process.env.PORT || 8001);
