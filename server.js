f6b9a4c2e1d60f254e5e5756c66051c1f24c6a98
Eric Wendelin <emwendelin@gmail.com>
2011-03-19 11:07:19 -0600

    Adding debugging for exceptions

--- server.js
+++ server.js
@@ -7,17 +7,8 @@ app.configure(function(){
     app.use(express.methodOverride());
     app.use(express.bodyParser());
     app.use(app.router);
-});
-
-app.configure('development', function(){
     app.use(express.static(__dirname + '/public'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  var oneYear = 31557600000;
-  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
 });
 
 var subject = new Subject();
