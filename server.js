bc5dd24f0f1506cb60573a747d97c55d6e0fdbed
Eric Wendelin <emwendelin@gmail.com>
2011-03-20 09:04:56 -0600

    Adding production config

--- server.js
+++ server.js
@@ -1,34 +1,41 @@
 var express = require('express'),
-     pub = __dirname + '/public',
-     port = process.env.PORT || 8001;
+    pub = __dirname + '/public',
+    port = process.env.PORT || 8001;
 
 var Subject = require('./models/subject').Subject;
 var subject = new Subject();
 
 var app = express.createServer();
 app.get('/', function(req, res) {
-     subject.findAll(function(error, subjects) {
-          res.render('index', {
-               locals: {
-                    pageTitle: 'Do we have a quorum?',
-                    subjects: subjects
-               }
-          });
-     });
-     response.end();
+    subject.findAll(function(error, subjects) {
+        res.render('index', {
+            locals: {
+                pageTitle: 'Do we have a quorum?',
+                subjects: subjects
+            }
+        });
+    });
+    response.end();
 });
 
 app.configure(function() {
-     app.use(require('stylus').middleware(pub));
-     app.use(express.logger());
-     app.set('views', __dirname + '/views');
-     app.set('view engine', 'jade');
+    app.use(require('stylus').middleware(pub));
+    app.use(express.logger());
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
 });
 
 app.configure('development', function() {
-     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-     app.use(express.static(pub));
-     app.set('view options', { scope: { development: true }});
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.static(pub));
+    app.set('view options', { scope: { development: true }});
+});
+
+// Joyent smartmachines setup as production by default
+app.configure('production', function() {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.static(pub));
+    app.set('view options', { scope: { development: true }});
 });
 
 app.listen(port);
