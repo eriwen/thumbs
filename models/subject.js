c28924af2b10558ed23bd023bc985e7f7cf3ff6e
Eric Wendelin <emwendelin@gmail.com>
2011-03-20 15:24:38 -0600

    using route separation

--- models/subject.js
+++ models/subject.js
@@ -1,44 +1,47 @@
-var subjectCounter = 1;
-
-Subject = function(){};
-Subject.prototype.dummyData = [];
-
-Subject.prototype.findAll = function(callback) {
-	callback(null, this.dummyData);
-};
-
-Subject.prototype.findById = function(id, callback) {
-	var result = null;
-	for (var i = 0, len = this.dummyData.length; i < len; i++) {
-		if (this.dummyData[i]._id == id) {
-			result = this.dummyData[i];
-			break;
-		}
-	}
-	callback(null, result);
-}
-
-Subject.prototype.save = function(subjects, callback) {
-	var subject = null;
-	
-	for(var i = 0, len = subjects.length; i < len; i++) {
-		subject = subjects[i];
-		subject._id = subjectCounter++;
-		if (!subject.notes) {
-			subject.notes = [];
-		}
-		this.dummyData.push(subject);
-	}
-	
-	callback(null, subjects);
-}
-
-new Subject().save([
+var subjects = [
 	{name: 'Fred Jean', rating: 10.0, notes: [
 		{author: 'Eric Wendelin', note: 'JRubyist', rating: 3.0},
 		{author: 'Johnny Wey', note: 'Is a genius', rating: 9.0},
 	]},
 	{name: 'John Doe', rating: 5.4}
-], function(error, subjects){});
+];
 
-exports.Subject = Subject;
+exports.list = function(req, res) {
+	res.render('subjects', { title: 'Subjects', subjects: subjects });
+};
+
+exports.create = function(req, res, next) {
+	var id = req.params.id;
+	req.subject = subjects[id];
+	if (req.subject) {
+		next();
+	} else {
+		next(new Error('cannot find subject ' + id));
+	}
+};
+
+exports.read = function(req, res) {
+	res.render('subjects/read', {
+		title: 'Subject: ' + req.subject.name,
+		subject: req.subject
+	});
+};
+
+exports.edit = function(req, res) {
+	res.render('subjects/edit', {
+		title: 'Editing subject: ' + req.subject.name,
+		subject: req.subject
+	});
+};
+
+exports.update = function(req, res) {
+	// TODO: validation and DB insert
+	var subject = req.body.subject;
+	req.subject.name = subject.name;
+	res.redirect('back');
+};
+
+exports.delete = function(req, res) {
+	// TODO: delete subject from subjects
+	res.redirect('back');
+}
--- server.js
+++ server.js
@@ -1,62 +1,30 @@
 var express = require('express'),
+	app = express.createServer(),
     pub = __dirname + '/public',
+	subject = require('./models/subject'),
+	// vote = require('./models/vote'),
+	site = require('./site'),
     port = process.env.PORT || 8001;
 
-var Subject = require('./models/subject').Subject;
-var subject = new Subject();
-
-var app = express.createServer();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(require('stylus').middleware(pub));
+app.use(express.logger());
 // NOTE: must include there here and NOT in app.config or req.body will always be undefined!
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
-app.get('/', function(req, res) {
-    subject.findAll(function(error, subjects) {
-        res.render('index', {
-            locals: {
-                pageTitle: 'Quorum',
-                subjects: subjects
-            }
-        });
-    });
-});
+app.get('/', site.index);
 
-app.get('/subject/:id', function(req, res, next) {
-	subject.findById(req.params.id, function(err, subject) {
-		if (err) return next(err);
-		res.render('subject', {
-			locals: {
-				subject: subject
-			}
-		});
-	});
-});
+app.all('/subjects', subject.list);
+app.all('/subject/:id/:op?', subject.load);
+app.get('/subject/:id', subject.read);
+app.get('/subject/:id/read', subject.read);
+app.post('/subject/:id/create', subject.create);
+app.get('/subject/:id/update', subject.edit);
+app.put('/subject/:id/update', subject.update);
+app.del('/subject/:id/delete', subject.delete);
 
-app.post('/subject/create', function(req, res) {
-	var newSubject = null;
-	subject.save([{name: req.body.name}], function(error, subject) {
-		res.redirect('/');
-	});
-});
-
-app.configure(function() {
-    app.use(require('stylus').middleware(pub));
-    app.use(express.logger());
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-});
-
-app.configure('development', function() {
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.use(express.static(pub));
-    app.set('view options', { scope: { development: true }});
-});
-
-// Joyent smartmachines setup as production by default
-app.configure('production', function() {
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.use(express.static(pub));
-    app.set('view options', { scope: { development: true }});
-});
+// app.post('/vote/:id/create', vote.create);
 
 app.listen(port);
