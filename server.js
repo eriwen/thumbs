d1b618812b545e68f0b44c253059db7f1c4c2aba
Eric Wendelin <emwendelin@gmail.com>
2011-03-21 07:40:36 -0600

    Explicitly compiling CSS

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 var express = require('express'),
+	stylus = require('stylus'),
 	app = express.createServer(),
     pub = __dirname + '/public',
+	views = __dirname + '/views',
 	subject = require('./models/subject'),
-	// vote = require('./models/vote'),
+	// note = require('./models/note'),
 	site = require('./site'),
     port = process.env.PORT || 8001;
 
-app.set('views', __dirname + '/views');
+function compileCss(str, path, compress, fn) {
+	stylus(str).set('filename', path).set('compress', compress).render(fn);
+}
+
+app.use(stylus.middleware({src: pub + '/stylesheets', dest: pub, compile: compileCss}));
+app.set('views', views);
 app.set('view engine', 'jade');
 app.use(express.logger());
 // NOTE: must include there here and NOT in app.config or req.body will always be undefined!
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-
-app.configure(function() {
-	app.use(require('stylus').middleware(pub));
-});
 
 app.get('/', site.index);
 
@@ -23,11 +26,11 @@ app.all('/subject', subject.list);
 app.all('/subject/:id/:op?', subject.load);
 app.get('/subject/:id', subject.read);
 app.get('/subject/:id/read', subject.read);
-app.post('/subject/:id/create', subject.create);
+app.put('/subject/:id/create', subject.create);
 app.get('/subject/:id/update', subject.edit);
-app.put('/subject/:id/update', subject.update);
+app.post('/subject/:id/update', subject.update);
 app.get('/subject/:id/delete', subject.delete);
 
-// app.post('/vote/:id/create', vote.create);
+// app.put('/note/:id/create', vote.create);
 
 app.listen(port);
