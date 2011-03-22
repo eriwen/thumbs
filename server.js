ae9e005e48ee3f7842b4f04b85283881455a6542
Eric Wendelin <emwendelin@gmail.com>
2011-03-21 19:35:27 -0600

    Fixing typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require('express'),
 
 app.use(stylus.middleware({
 	src: __dirname + '/views',
-	dest: __dirname + '/public',
+	dest: __dirname + '/public'
 }));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
