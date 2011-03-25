6e204beded59f8506f977ad06c7b9ae47b2b2e7a
Eric Wendelin <emwendelin@gmail.com>
2011-03-24 18:45:39 -0600

    Fixing typo in model

--- models/subject.js
+++ models/subject.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mydb');
 var Schema = mongoose.Schema,
 	ObjectId = Schema.ObjectId,
-	Note = require('./note');
+	Note = require('./note').Note;
 
 var Subject = new Schema({
 	name: {type: String, default: '', required: true},
