468d4c735cb42f1b90fb69b504aa1a83415d650c
Eric Wendelin <emwendelin@gmail.com>
2011-03-26 12:52:41 -0600

    Updating subject list

--- controllers/subject.js
+++ controllers/subject.js
@@ -14,7 +14,7 @@ function computeRating(subject) {
 
 exports.list = function(req, res) {
 	Subject.find({}, function(err, objs) {
-		res.render('subject', { title: 'Subjects', subjects: objs });
+		res.render('subject', { title: 'Thumbs', subjects: objs });
 	});
 };
 
