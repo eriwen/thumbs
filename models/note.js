d84edfce0b4963526e02db5609e135976e183c82
Eric Wendelin <emwendelin@gmail.com>
2011-03-26 10:39:55 -0600

    Moving Note model into Subject

--- models/note.js
+++ models/note.js
@@ -3,11 +3,4 @@ mongoose.connect('mongodb://localhost/mydb');
 var Schema = mongoose.Schema,
 	ObjectId = Schema.ObjectId;
 
-var Note = new Schema({
-	author: {type: String, default: '', required: true},
-	rating: {type: Number, default: 0, required: true},
-	note: {type: String, default: ''}
-});
-  
-mongoose.model('Note', Note);
-var Note = exports.Note = mongoose.model('Note');
+
--- models/subject.js
+++ models/subject.js
@@ -2,7 +2,16 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mydb');
 var Schema = mongoose.Schema,
 	ObjectId = Schema.ObjectId,
-	Note = require('./note').Note;
+
+// Subject can have 0..n Notes
+var Note = new Schema({
+	author: {type: String, default: '', required: true},
+	rating: {type: Number, default: 0, required: true},
+	note: {type: String, default: ''}
+});
+
+mongoose.model('Note', Note);
+var Note = exports.Note = mongoose.model('Note');
 
 var Subject = new Schema({
 	name: {type: String, default: '', required: true},
