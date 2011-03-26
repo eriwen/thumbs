77a0e29816aa3e0ba488df6c8c792d05a0ffcb05
Eric Wendelin <emwendelin@gmail.com>
2011-03-26 14:35:58 -0600

    Updating add note stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
 	var subjects = $('.subject');
 	
-	subjects.click(function() {
+	function clickSubject() {
 		var self = $(this);
 		subjects.removeClass('active');
 		self.addClass('active');
+
 		$.get('/subject/' + self.attr('rel'), function(data) {
-			self.append(data);
-			// TODO: rewrite function to just close
+			self.children('.detail').append(data);
 		});
-	});	
+		
+		clickSubject = function() {
+			var self = $(this);
+			subjects.removeClass('active');
+			self.addClass('active');
+		}
+	}
+	
+	subjects.click(clickSubject);
 });
