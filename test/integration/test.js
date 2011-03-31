6de793f5badeda1a9dd5739863311f38871073e0
Eric Wendelin <emwendelin@gmail.com>
2011-03-30 20:42:34 -0600

    Adding integration tests

--- test/integration/test.js
+++ test/integration/test.js
@@ -1,10 +1,38 @@
 var zombie = require('zombie'),
-	assert = require('assert');
+	assert = require('assert'),
+	Subject = require('../../models/subject').Subject;
 
 zombie.visit('http://localhost', {debug: true}, function(err, browser) {
 	if (err) {
 		console.log(err.message);
 	} else {
-		console.log(browser.html());
+		var testSubjectName = 'Test subject';
+		browser.fill('#newsubject input[type=text]', testSubjectName);
+		browser.pressButton("Create New", function(err, browser, status) {
+			if (err) console.log(err);
+			assertSubjectExists(testSubjectName);
+			deleteTestSubjects(testSubjectName);
+			// Object .subject:last has no method 'dispatchEvent'
+			browser.fire('click', '.subject:last', function() {
+				console.log('yaysauce');
+			});
+		});
+		console.log('done');
 	}
 });
+
+function assertSubjectExists(name) {
+	Subject.findOne({'name': name}, function(err, doc) {
+		assert.ok(doc);
+	});
+}
+
+function deleteTestSubjects(name) {
+	Subject.find({'name': name}, function(err, docs) {
+		for (var i = 0; i < docs.length; i++) {
+			docs[i].remove(function(err) {
+				if (err) console.log(err);
+			});
+		}
+	});
+}
