de0c5e82c28249c4ad1f9c8905a094a9e509f160
Eric Wendelin <emwendelin@gmail.com>
2011-04-02 11:39:10 -0600

    Adding integration tests for ratings

--- test/integration/test.js
+++ test/integration/test.js
@@ -2,29 +2,43 @@ var zombie = require('zombie'),
 	assert = require('assert'),
 	Subject = require('../../models/subject').Subject;
 
-zombie.visit('http://localhost', {debug: true}, function(err, browser) {
-	if (err) {
-		browser.log(err);
-	} else {
-		var testSubjectName = 'Test subject';
-		browser.fill('#newsubject input[type=text]', testSubjectName);
-		browser.pressButton("Create New", function(err, browser, status) {
-			if (err) browser.log(err);
-			assertSubjectExists(testSubjectName);
-			var newSubject = browser.querySelector('.subject:last');
-			browser.fire('click', newSubject, function() {
-				browser.log('yaysauce');
-				browser.log(browser.html('.subject:last'));
-			});
-			deleteTestSubjects(testSubjectName);
-		});
-		browser.log('done');
-	}
-});
+zombie.visit('http://localhost', {debug: true}, runTest);
+
+function runTest(err, browser) {
+	if (err) browser.log(err);
+	else createNewSubject(browser);
+}
+
+function createNewSubject(browser) {
+	browser.fill('#newsubject input[type=text]', 'Test subject');
+	browser.pressButton('Create New', verifyNewSubjectAndAddNote);
+}
+
+function verifyNewSubjectAndAddNote(err, browser, status) {
+	if (err) browser.log(err);
+	assertSubjectExists('Test subject');
+	browser.fire('click', browser.querySelector('.subject:last div.stars img:last'), verifyRating);
+	browser.wait(function addNoteCallback() {
+		browser.fire('click', browser.querySelector('.subject:last'), fillAndSubmitAddNoteForm);
+	});
+}
 
 function assertSubjectExists(name) {
 	Subject.findOne({'name': name}, function(err, doc) {
 		assert.ok(doc);
+	});
+}
+
+function verifyRating(err, browser) {
+	var ratingInput = browser.querySelector('.subject:last div.stars input[type="hidden"]');
+	assert.equal(ratingInput.getAttribute('value'), '5');
+}
+
+function fillAndSubmitAddNoteForm(err, browser) {
+	if (err) browser.log(err);
+	browser.wait(function(err, browser) {
+		browser.log(browser.html('.subject:last'));
+		deleteTestSubjects('Test subject');
 	});
 }
 
