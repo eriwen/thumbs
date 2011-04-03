3c6f1d29c28289e07d28d2e8f64ee6a5ec050b54
Eric Wendelin <emwendelin@gmail.com>
2011-04-03 09:50:22 -0600

    Debugging integration tests

--- test/integration/test.js
+++ test/integration/test.js
@@ -16,53 +16,74 @@ function runTest(err, browser) {
 }
 
 function createNewSubject(browser, callback) {
+	browser.log('createNewSubject()');
 	browser.fill('#newsubject input[type="text"]', 'Test subject');
 	browser.pressButton('Create New', callback);
+	browser.log('end createNewSubject()');
 }
 
 function assertSubjectExists(err, browser) {
+	browser.log('assertSubjectExists()');
 	Subject.findOne({'name': 'Test subject'}, function(err, doc) {
 		assert.ok(doc);
 	});
-}
-
-function rate(err, browser) {
-	if (err) browser.log(err);
-	browser.fire('click', browser.querySelector('.subject:last div.stars img:last'), verifyRating);
-}
-
-function verifyRating(err, browser) {
-	if (err) browser.log(err);
-	var ratingInput = browser.querySelector('.subject:last div.stars input[type="hidden"]');
-	assert.equal(ratingInput.getAttribute('value'), '5');
+	browser.log('end assertSubjectExists()');
 }
 
 function clickSubject(err, browser) {
+	browser.log('clickSubject()');
 	if (err) browser.log(err);
 	browser.fire('click', browser.querySelector('.subject:last'), verifyOpenSubject);
+	browser.log('end clickSubject()');
 }
 
-function verifyOpenSubject(err, browser) {
+function verifyOpenSubject(err, browser, status) {
+	browser.log('verifyOpenSubject()');
 	if (err) browser.log(err);
-	assert.equal(browser.title(), 'Test subject');
+	browser.log('status ' + status);
+	assert.equal(browser.text('title'), 'Test subject');
+	browser.log('end verifyOpenSubject()');
+}
+
+function rate(err, browser) {
+	browser.log('rate()');
+	if (err) browser.log(err);
+	browser.wait(function(err, browser) {
+		browser.log('html ' + browser.html());
+		browser.fire('click', browser.querySelector('.stars img:last'), verifyRating);
+	});
+	browser.log('end rate()');
+}
+
+function verifyRating(err, browser) {
+	browser.log('verifyRating()');
+	if (err) browser.log(err);
+	var ratingInput = browser.querySelector('.stars input[type="hidden"]');
+	assert.equal(ratingInput.getAttribute('value'), '5');
+	browser.log('end verifyRating()');
 }
 
 function addNote(err, browser) {
+	browser.log('addNote()');
 	if (err) browser.log(err);
 	browser.wait(function(err, browser) {
 		if (err) browser.log(err);
 		browser.fill('.addnote .note', 'test note');
 		browser.pressButton('Add Note', verifyNewNote);
 	});
+	browser.log('end addNote()');
 }
 
 function verifyNewNote(err, browser) {
+	browser.log('verifyNewNote()');
 	if (err) browser.log(err);
 	var newNote = browser.querySelector('.notes li');
 	assert.equal(newNote.innerHTML, 'test note');
+	browser.log('end verifyNewNote()');
 }
 
 function deleteTestSubjects(err, browser) {
+	browser.log('deleteTestSubjects()');
 	Subject.find({'name': 'Test subject'}, function(err, docs) {
 		for (var i = 0; i < docs.length; i++) {
 			docs[i].remove(function(err) {
@@ -70,4 +91,5 @@ function deleteTestSubjects(err, browser) {
 			});
 		}
 	});
+	browser.log('end deleteTestSubjects()');
 }
