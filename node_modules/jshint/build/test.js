7ad31a9c177d33ac4eb99bac04300d18cb77c3c1
Eric Wendelin <emwendelin@gmail.com>
2011-05-24 22:33:08 -0600

    Moving testing bits around for Jenkins

---
+++ node_modules/jshint/build/test.js
@@ -0,0 +1,26 @@
+module.exports = function () {
+    var jasmine = require('jasmine-node'),
+        verbose = false,
+        colored = true,
+        key;
+
+    for (key in jasmine) {
+        if (Object.prototype.hasOwnProperty.call(jasmine, key)) {
+            global[key] = jasmine[key];
+        }
+    }
+
+    process.argv.forEach(function (arg) {
+        switch (arg) {
+        case '--no-color':
+            colored = false;
+            break;
+        case '--silent':
+            verbose = false;
+            break;
+        }
+    });
+
+    jasmine.executeSpecsInFolder(__dirname + "/../test/", function (runner, log) {
+    }, verbose, colored);
+};
